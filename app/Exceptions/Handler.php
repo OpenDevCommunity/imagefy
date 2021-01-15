82396e545a60d817b7417a0faff53aee4a1668ea
Marek Dev <marek@marekdev.me>
2021-01-15 13:05:48 +0000

    Add errors table & fix version commit

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use App\Models\Error;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -34,7 +35,14 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            //
+            Error::create([
+                'user_id' => \Auth::user() ? \Auth::id() : null,
+                'code' => $e->getCode(),
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+                'message' => $e->getMessage(),
+                'trace' => $e->getTraceAsString()
+            ]);
         });
     }
 }
