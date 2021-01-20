d3f021ae28c1059edf9ad6d2c1b5c5a635f85bff
Marek Dev <marek@marekdev.me>
2021-01-20 20:51:26 +0000

    Add auto refresh indicator

--- resources/views/livewire/user/recent-images.blade.php
+++ resources/views/livewire/user/recent-images.blade.php
@@ -3,8 +3,8 @@
         <div class="card-body">
             <a href="{{ route('user.image.library') }}" class="btn btn-success btn-sm float-right">View All</a>
             <button wire:click="getUserImages()" style="margin-right: 5px;" class="btn btn-success btn-sm float-right">Refresh</button>
-            <h5>Your recently uploaded images (5)</h5>
-            <span class="text-muted">Here you can see your recently uploaded images (5)</span>
+            <h5>Your recently uploaded images &ensp;  <span wire:loading wire:target="getUserImages"><i class="fas fa-sync fa-spin"></i></span></h5>
+            <span class="text-muted">Here you can see your recently uploaded images</span>
             <hr>
             <div class="table-responsive" wire:loading.remove wire:target="delete">
                 <table class="table">
